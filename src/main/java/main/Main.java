c1833079051c18effd56f75e1e4c8a35301a5265
evglevin <levin.inf@gmail.com>
2017-10-24 01:28:31 +0300

    Authentication added

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import org.eclipse.jetty.security.JDBCLoginService;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        WebAppContext ctx = new WebAppContext();
+        ctx.setResourceBase("src/main/html");
+
+        JDBCLoginService loginService = new JDBCLoginService("JCGRealm", "properties/jdbcrealm_Postgres.properties");
+
+//        HashLoginService loginService = new HashLoginService("JCGRealm");
+//        loginService.setConfig("jcgrealm.txt");
+
+        server.addBean(loginService);
+
+        server.setHandler(ctx);
+
+        server.start();
+        server.join();
+    }
+}
